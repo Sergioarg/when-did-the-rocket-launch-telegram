8254efe8881c38947d739df9d9d9ab679c3a3792
sergioarg <sergio_ramos53@yahoo.com>
2024-03-26 22:56:02 -0500

    Change functions

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ def main():
     telebot = TelegramBot()
     telebot.bot.message_handler(commands=['start'])(telebot.send_welcome)
     telebot.bot.message_handler(commands=['help'])(telebot.send_help)
-    telebot.bot.message_handler(commands=['launch'])(telebot.send_options)
+    telebot.bot.message_handler(commands=['launch'])(telebot.send_confirmation_options)
     telebot.bot.callback_query_handler(func=lambda call: True)(telebot.callback_query)
     print('Bot iniciado correctamente.')
     telebot.bot.polling(none_stop=True)
